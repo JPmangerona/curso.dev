e3c67aeb3b20377c7c689c1615d01d52fcd6781f
JPmangerona <joaopedromangerona@edu.unifil.br>
2025-12-26 06:15:40 +0000

    formatador instalado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home(){
-    return <h1>vamos ver se muda algo</h1>
+  return <h1>vamos ver se muda algo</h1>
 }
+
+function teste2() {
+    console.log("indentação errada");
+}
+
+
 
 export default Home;
